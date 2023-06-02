f2ad496fb5e4130c5900ca6bc7c0020957779626
John Lenz <john.lenz27@gmail.com>
2023-06-02 16:37:42 -0700

    added probelm 4 example

--- leet.js
+++ leet.js
@@ -95,3 +95,16 @@ var filter = function(arr, fn) {
 
 // Given an integer array nums, a reducer function fn, and an initial value init, return a reduced array.
 // A reduced array is created by applying the following operation: val = fn(init, nums[0]), val = fn(val, nums[1]), val = fn(val, nums[2]), ... until every element in the array has been processed. The final value of val is returned.
+
+// Input: 
+// nums = [1,2,3,4]
+// fn = function sum(accum, curr) { return accum + curr; }
+// init = 0
+// Output: 10
+// Explanation:
+// initially, the value is init=0.
+// (0) + nums[0] = 1
+// (1) + nums[1] = 3
+// (3) + nums[2] = 6
+// (6) + nums[3] = 10
+// The final answer is 10.
