6571e499549a219b6ae230398966ede824f103af
John Lenz <john.lenz27@gmail.com>
2023-06-02 16:14:42 -0700

    add input and output example

--- leet.js
+++ leet.js
@@ -70,5 +70,10 @@ var map = function(arr, fn) {
 
 
 // Given an integer array arr and a filtering function fn, return a new array with a fewer or equal number of elements.
-
 // The returned array should only contain elements where fn(arr[i], i) evaluated to a truthy value.
+
+// Input: arr = [0,10,20,30], fn = function greaterThan10(n) { return n > 10; }
+// Output: [20,30]
+// Explanation:
+// const newArray = filter(arr, fn); // [20, 30]
+// The function filters out values that are not greater than 10
