276c6d00271e0b61edcf634977cc42dcd0ffd006
John Lenz <john.lenz27@gmail.com>
2023-06-06 20:49:32 -0700

    new question

--- leet.js
+++ leet.js
@@ -154,3 +154,11 @@ var argumentsLength = function(...args) {
 var argumentsLength = function(...args) { //return without loop to use less memory
     return args.length;
   };
+
+//  You are given an array items, where each items[i] = [typei, colori, namei] describes the type, color, and name of the ith item. You are also given a rule represented by two strings, ruleKey and ruleValue.
+
+// The ith item is said to match the rule if one of the following is true:
+
+// ruleKey == "type" and ruleValue == typei.
+// ruleKey == "color" and ruleValue == colori.
+// ruleKey == "name" and ruleValue == namei.
