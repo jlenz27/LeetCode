6a696e9b930e46140e2fd4f9b121dfb9a6c88849
John Lenz <john.lenz27@gmail.com>
2023-06-06 21:15:15 -0700

    example

--- leet.js
+++ leet.js
@@ -194,3 +194,9 @@ var countMatches = function(items, ruleKey, ruleValue) {
 // Specifically, ans is the concatenation of two nums arrays.
 
 // Return the array ans
+
+// Input: nums = [1,2,1]
+// Output: [1,2,1,1,2,1]
+// Explanation: The array ans is formed as follows:
+// - ans = [nums[0],nums[1],nums[2],nums[0],nums[1],nums[2]]
+// - ans = [1,2,1,1,2,1]
