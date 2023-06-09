4e5b0679b4ae3c612b397c90e2432cea47026c35
John Lenz <john.lenz27@gmail.com>
2023-06-08 18:25:23 -0700

    update

--- leet.js
+++ leet.js
@@ -287,3 +287,10 @@ var sortSentence = function (s) {
 // Given two string arrays word1 and word2, return true if the two arrays represent the same string, and false otherwise.
 
 // A string is represented by an array if the array elements concatenated in order forms the string.
+
+// Input: word1 = ["ab", "c"], word2 = ["a", "bc"]
+// Output: true
+// Explanation:
+// word1 represents string "ab" + "c" -> "abc"
+// word2 represents string "a" + "bc" -> "abc"
+// The strings are the same, so return true.
