24f91109657b4d5c38337911fa2a831bcba9e7d6
John Lenz <john.lenz27@gmail.com>
2023-06-08 20:34:16 -0700

    answer

--- leet.js
+++ leet.js
@@ -343,7 +343,14 @@ var numIdenticalPairs = function(nums) {
 };
 
 // Given a positive integer n, return the smallest positive integer that is a multiple of both 2 and n.
-
 // Input: n = 5
 // Output: 10
 // Explanation: The smallest multiple of both 5 and 2 is 10.
+
+var smallestEvenMultiple = function(n) {
+    if(n % 2 != 0){
+        return n * 2
+    }else{
+     return (n * 2)/2
+    }
+};
