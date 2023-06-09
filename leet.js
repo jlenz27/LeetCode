6514a20b7dcee46b5f6ac954d97825297396507b
John Lenz <john.lenz27@gmail.com>
2023-06-08 21:27:53 -0700

    answer

--- leet.js
+++ leet.js
@@ -385,3 +385,19 @@ var subtractProductAndSum = function (n) {
 // For nums[2]=2 there exist one smaller number than it (1). 
 // For nums[3]=2 there exist one smaller number than it (1). 
 // For nums[4]=3 there exist three smaller numbers than it (1, 2 and 2).
+
+var smallerNumbersThanCurrent = function (nums) {
+    let arr = [];
+    for (let i = 0; i < nums.length; i++) {
+        let count = 0;
+        for (let j = 0; j < nums.length; j++) {
+            if (nums[j] < nums[i]) {
+                count++
+
+            }
+
+        }
+        arr.push(count)
+    }
+    return arr
+};
