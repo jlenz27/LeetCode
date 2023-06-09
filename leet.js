e956f74e2fa9ad1f03ae4e91958b39e707190a05
John Lenz <john.lenz27@gmail.com>
2023-06-09 14:38:08 -0700

    example

--- leet.js
+++ leet.js
@@ -438,3 +438,9 @@ var debounce = function(fn, t) {
 // For example, "Hello World", "HELLO", and "hello world hello world" are all sentences.
 // You are given a sentence s​​​​​​ and an integer k​​​​​​. You want to truncate s​​​​​​ such that it contains only the first k​​​​​​ words. Return s​​​​​​ after truncating it.
 
+// Input: s = "Hello how are you Contestant", k = 4
+// Output: "Hello how are you"
+// Explanation:
+// The words in s are ["Hello", "how" "are", "you", "Contestant"].
+// The first 4 words are ["Hello", "how", "are", "you"].
+// Hence, you should return "Hello how are you".
