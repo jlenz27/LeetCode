3d04286a3ba191f40825271d08283ce6e6544e8f
John Lenz <john.lenz27@gmail.com>
2023-06-08 18:39:32 -0700

    answer

--- leet.js
+++ leet.js
@@ -317,3 +317,8 @@ var arrayStringsAreEqual = function(word1, word2) { //A one line solution to the
 // Input: celsius = 36.50
 // Output: [309.65000,97.70000]
 // Explanation: Temperature at 36.50 Celsius converted in Kelvin is 309.65 and converted in Fahrenheit is 97.70.
+
+var convertTemperature = function(celsius) {
+    let an =[celsius + 273.15, celsius * 1.80 + 32.00]
+    return an
+};
