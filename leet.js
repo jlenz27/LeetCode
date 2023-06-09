56af23f70f09d2bc1fd543456ed847d0c1465c7f
John Lenz <john.lenz27@gmail.com>
2023-06-09 14:43:58 -0700

    example

--- leet.js
+++ leet.js
@@ -458,6 +458,8 @@ var truncateSentence = function(s, k) {
 //  A pangram is a sentence where every letter of the English alphabet appears at least once.
 // Given a string sentence containing only lowercase English letters, return true if sentence is a pangram, or false otherwise.
 
-
+// Input: sentence = "thequickbrownfoxjumpsoverthelazydog"
+// Output: true
+// Explanation: sentence contains at least one of every letter of the English alphabet.
 
  
