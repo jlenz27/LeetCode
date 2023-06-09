c76902843d247e12dae1788dc4a8cce74cb2eb7e
John Lenz <john.lenz27@gmail.com>
2023-06-08 17:47:24 -0700

    test

--- leet.js
+++ leet.js
@@ -256,3 +256,8 @@ var shuffle = function(nums, n) {
 
 // A sentence is a list of words that are separated by a single space with no leading or trailing spaces. Each word consists of lowercase and uppercase English letters.
 // A sentence can be shuffled by appending the 1-indexed word position to each word then rearranging the words in the sentence.
+
+// Input: s = "is2 sentence4 This1 a3"
+// Output: "This is a sentence"
+// Explanation: Sort the words in s to their original positions "This1 is2 a3 sentence4", then remove the numbers.
+
