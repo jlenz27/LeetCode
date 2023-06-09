b7df8eee5b0b79e66323872f86e11d5458d565a9
John Lenz <john.lenz27@gmail.com>
2023-06-08 18:50:28 -0700

    solution

--- leet.js
+++ leet.js
@@ -325,3 +325,19 @@ var convertTemperature = function(celsius) {
 
 // Given an array of integers nums, return the number of good pairs.
 // A pair (i, j) is called good if nums[i] == nums[j] and i < j.
+
+// Input: nums = [1,2,3,1,1,3]
+// Output: 4
+// Explanation: There are 4 good pairs (0,3), (0,4), (3,4), (2,5) 0-indexed.
+
+var numIdenticalPairs = function(nums) {
+    let counter = 0
+    for(let i = 0; i < nums.length;i++){
+        for(let j = 0; j < nums.length;j++){
+        if(nums[i] == nums[j] && (i < j)){
+            counter++
+        }
+        }
+    }
+    return counter
+};
