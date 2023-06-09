2e5dba205944b57ce5bfd721ce966eb5e3c7e625
John Lenz <john.lenz27@gmail.com>
2023-06-08 21:03:48 -0700

    question

--- leet.js
+++ leet.js
@@ -354,3 +354,6 @@ var smallestEvenMultiple = function(n) {
      return (n * 2)/2
     }
 };
+
+// Given an integer number n, return the difference between the product of its digits and the sum of its digits.
+ 
